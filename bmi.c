f83f03c7ef591fd4e6e6bcc572a89d27c1ee732e
장호윤 <lg55421844@gmail.com>
2023-04-06 05:05:39 +0000

    bmi 계산기

---
+++ bmi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	
+	float height = 184.4;
+	float waight = 90.5;
+	float bmi = 0;
+	height = height * 0.01;
+	bmi = weight / (height * height);
+	printf("당신의 BMI ; %.2f\n", bmi);
+	
+	return 0;
+}
