efd29fcf81b2ad23fa64a4fff145dd12de9a0dc4
장호윤 <lg55421844@gmail.com>
2023-04-06 05:16:21 +0000

    47b.c

---
+++ 47b.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+ int main()
+ {
+ int num1=16;
+ int num2;
+ int sum=0; int sub=0;
+ num2 = 61;
+ sum = num1 + num2;
+ printf("%d + %d = %d",num1,num2,sum);
+ sum = sum1 + num2;
+ sub = num1 - num2;
+ mul = num1 * num2;
+ div = num1 / num2;
+ printf("%d + %d = %d\n",num1 ,num2 ,sum);
+ printf("%d - %d = %d\n",num1 ,num2 ,sud);
+ printf("%d * %d = %d\n",num1 ,num2 ,mul);
+ printf("%d / %d = %d\n",num1 ,num2 ,div);
+	 
+	 return 0;
+ }
+ 
