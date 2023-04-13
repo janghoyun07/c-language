3fa036d16403b5cc2a790caa2d3ec9c125933a90
장호윤 <lg55421844@gmail.com>
2023-04-13 06:23:55 +0000

    54.c

---
+++ calc/54.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+
+
+*/
+int main()
+{
+	int a, a1, a2;  //변수명은 숫자가 앞에 올수 없다.
+	int A, cat, Cat, cAt, caT;
+	int _a, __a, ___;
+	int if;//예약어 변수명으로 사욜할 수 없다.
+	
+	return 0;
+}
